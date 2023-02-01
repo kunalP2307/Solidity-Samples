e2d8e4375937d067db6b38f66cf5c8384673641b
kunalP2307 <kunalpatil2307@gmail.com>
2023-02-01 23:25:11 +0530

    Simple ERC20 Token

---
+++ ERC20Token.sol
@@ -0,0 +1,14 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PTLToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Patil", "PTL") {
+
+        // used to create new tokens
+        _mint(msg.sender, initialSupply);
+    }
+}    
+
